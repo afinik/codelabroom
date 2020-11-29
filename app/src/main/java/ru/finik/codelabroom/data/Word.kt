a66cdacfb733ca412f60f40f85fec2233565a973
smilex <alexdate@gmail.com>
2020-11-30 01:23:11 +0300

    first commit

---
+++ app/src/main/java/ru/finik/codelabroom/data/Word.kt
@@ -0,0 +1,10 @@
+package ru.finik.codelabroom.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//data class Word(val word: String)
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
